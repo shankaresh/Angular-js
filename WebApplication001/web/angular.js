d373c20c6f9bd7f57858dbf029d202bf622065f5
shankaresh <shankargowda0511@gmail.com>
2019-09-28 15:26:09 +0530

    Update angular.js

--- WebApplication001/web/angular.js
+++ WebApplication001/web/angular.js
@@ -6,17 +6,26 @@
 
 var app = angular.module('WebApplication001',[]);
 app.controller('ctrlwebapplication', function($scope){
-    $scope.fnamewebapp = "";
-    $scope.lnamewebapp = "";
-    $scope.Gender = {male:true,female:false};
-    $scope.departmentNames =[
-		{name: "B.Tech - IT"},
-		{name: "B.E - CSE"},
-		{name: "B.E - ECE"},
-            {name: "B.E - MECH"},
-        {name: "B.E - CIVIL"}];	
-    $scope.namewebapp = function(fnamewebapp,lnamewebapp)
+    $scope.fname="";
+    $scope.lname="";
+    $scope.email="";
+    $scope.departmentNames =[{name: "B.Tech - IT"},
+	{name: "B.E - CSE"},
+	{name: "B.E - ECE"},
+        {name: "B.E - MECH"},
+        {name: "B.E - CIVIL"}];
+    $scope.bgrp ={
+        option1 : "A and other",
+        option2 : "B and other",
+        option3 : "AB and other",
+        option4 : "O and other"
+    };
+    $scope.name = function(fname,lname)
     {
-        return fnamewebapp +" "+lnamewebapp;
-    }
+        return fname+" "+lname;
+    };
+    $scope.master = {};
+    $scope.save= function(emp) {
+        $scope.master = angular.copy(emp);
+    };
 });
